77c7115bfbcd56a14ecccf32e4221e5fade89a10
Aliaksandr Valialkin <valyala@gmail.com>
2018-04-03 22:24:49 +0300

    Improved example for BuildDict

--- dict_example_test.go
+++ dict_example_test.go
@@ -9,7 +9,7 @@ func ExampleBuildDict() {
 	// Collect samples for the dictionary.
 	var samples [][]byte
 	for i := 0; i < 1000; i++ {
-		sample := fmt.Sprintf("this is a sample number %d", i)
+		sample := fmt.Sprintf("this is a dict sample number %d", i)
 		samples = append(samples, []byte(sample))
 	}
 
@@ -18,29 +18,41 @@ func ExampleBuildDict() {
 
 	// Now the dict may be used for compression/decompression.
 
-	plainData := []byte("this is a data for the compression with dict")
-
-	// Compress data with the dict.
+	// Created CDict from the dict.
 	cd, err := NewCDict(dict)
 	if err != nil {
 		log.Fatalf("cannot create CDict: %s", err)
 	}
 	defer cd.Release()
-	compressedData := CompressDict(nil, plainData, cd)
+
+	// Compress multiple blocks with the same CDict.
+	var compressedBlocks [][]byte
+	for i := 0; i < 3; i++ {
+		plainData := fmt.Sprintf("this is line %d for dict compression", i)
+		compressedData := CompressDict(nil, []byte(plainData), cd)
+		compressedBlocks = append(compressedBlocks, compressedData)
+	}
 
 	// The compressedData must be decompressed with the same dict.
+
+	// Create DDict from the dict.
 	dd, err := NewDDict(dict)
 	if err != nil {
 		log.Fatalf("cannot create DDict: %s", err)
 	}
 	defer dd.Release()
-	decompressedData, err := DecompressDict(nil, compressedData, dd)
-	if err != nil {
-		log.Fatalf("cannot decompress data: %s", err)
+
+	// Decompress multiple blocks with the same DDict.
+	for _, compressedData := range compressedBlocks {
+		decompressedData, err := DecompressDict(nil, compressedData, dd)
+		if err != nil {
+			log.Fatalf("cannot decompress data: %s", err)
+		}
+		fmt.Printf("%s\n", decompressedData)
 	}
 
-	fmt.Printf("%s", decompressedData)
-
 	// Output:
-	// this is a data for the compression with dict
+	// this is line 0 for dict compression
+	// this is line 1 for dict compression
+	// this is line 2 for dict compression
 }
